cdc52be8df4e576523bc934ed55cbd709b45231d
SpecZ <39489547+aspect22@users.noreply.github.com>
2023-02-12 16:05:51 +0100

    Delete commit.bat

--- commit.bat
+++
@@ -1,7 +0,0 @@
-git init
-git add README.md
-git add -A && git commit -m "Your Message"
-git branch -M main
-git remote add origin git@github.com:aspect22/Credential-Generator.git
-git push -u origin main
-PAUSE
